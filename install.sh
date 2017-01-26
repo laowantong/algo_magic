5db8ab9856abecbce7b415f1eb08b7c3866da2be
laowantong <aristide@mac-mini-de-aristide-4.home>
2017-01-26 19:27:08 +0100

    add local repository

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PYTHON=~/anaconda/bin/python
+
+cd magic
+
+if [ -d "dist" ] ; then
+  rm -R "dist"
+fi
+
+if [ -d "build" ] ; then
+  rm -R "build"
+fi
+
+if [ -d "algo_magic.egg-info" ] ; then
+  rm -R "algo_magic.egg-info"
+fi
+
+$PYTHON setup.py install
+$PYTHON setup.py sdist
+$PYTHON setup.py bdist_wheel --universal
+
+# ~/anaconda/bin/twine upload dist/*
+
+echo "Done."
